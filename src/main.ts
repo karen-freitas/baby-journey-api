9c68b6afa68446753937907f0fd1f20f8060481d
Karen Freitas <karenfcorrea@gmail.com>
2025-05-19 20:44:42 -0300

    fix: remove commented code

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import * as yaml from 'js-yaml';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  // origin: process.env.URI_FRONTEND,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
